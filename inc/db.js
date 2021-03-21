33ef1f31fb8e8a5bda3c2a4140c14dc3fe03858d
maykoww <kiddeveloper5610@gmail.com>
2021-03-21 13:41:21 -0700

    👌

--- inc/db.js
+++ inc/db.js
@@ -3,6 +3,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: process.env.APP_DB_HOST,
     user: process.env.APP_DB_USER,
+    password: process.env.APP_DB_PASS,
     database: process.env.APP_DB_NAME,
     multipleStatements: true
 });
